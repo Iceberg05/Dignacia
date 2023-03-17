fc900bffefe6a2259b7a9cc67e77558a8aa72a95
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-03-17 11:41:49 +0300

    MagnumGun and gunChange has been created

---
+++ DignaciaGame/Assets/Yunus/Scripts/MKSGun/MKSShooting.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MKSShooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bullettPrefab;
+    public float bulletForce = 20f;
+
+    public static int ammo = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if(ammo > 0)
+            {
+                Shoot();
+            }
+            Debug.Log(ammo);
+
+        }
+    }
+    void Shoot(){
+        ammo = ammo -1 ;
+        GameObject bullet = Instantiate(bullettPrefab , firePoint.position , firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+    
+}
---
+++ DignaciaGame/Assets/Yunus/Scripts/MagnumGun/MagnumBulletScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnumBulletScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject , 2f);
+    }
+}
